48d62cd091bc99f320e997506f7f649cbe0834f2
RaphaelSodre <raphael_sodre@id.uff.br>
2024-07-03 12:31:51 +0000

    Endpoint Read All implementado

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Olá Mundo!')
+  res.send('Hello World!')
 })
+app.get('/oi', function (req, res) {
+  res.send('Olá Mundo!!')
+})
+
+// Lista de Personagnes
+const lista = ['Rick Sanches', 'Morty Smith', 'Summer Smith']
+
+// Read All - [GET] /item
+app.get('/item', function (req, res){
+  res.send(lista)
+})
+
 app.listen(3000)
